651cd9cf808cd06f7014f66b6085f17bd4e4f3c3
_ROBERT <robimitidoi@gmail.com>
2020-05-09 11:10:55 +0300

    First Interface

---
+++ app/src/main/java/com/example/optimmed/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.optimmed;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedBundleInstance) {
+        super.onCreate(savedBundleInstance);
+        setContentView(R.layout.login_activity);
+
+    }
+}
---
+++ app/src/main/java/com/example/optimmed/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.optimmed;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedBundleInstance) {
+        super.onCreate(savedBundleInstance);
+        setContentView(R.layout.register_activity);
+
+    }
+}
